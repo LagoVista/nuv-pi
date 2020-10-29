78b339a5b7f4419d64d2eec112869c64133ebe27
Kevin D. Wolf <kevinw@software-logistics.com>
2020-10-29 08:04:49 -0400

    v1.0.5 - remove ML from main library

--- nuvpy/__init__.py
+++ nuvpy/__init__.py
@@ -1,6 +1,5 @@
-import nuvpy.nuviot_auth
-import nuvpy.nuviot_data
-import nuvpy.nuviot_device
-import nuvpy.nuviot_ml
-import nuvpy.nuviot_srvc
-import nuvpy.nuviot_util
+from . import nuviot_auth
+from . import nuviot_data
+from . import nuviot_device
+from . import nuviot_srvc
+from . import nuviot_util
--- setup.py
+++ setup.py
@@ -5,16 +5,18 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="nuvpy", 
-    version="1.0.1",
+    version="1.0.5",
     author="Software Logistics, LLC",
     author_email="kevinw@software-logistics.com",
     description="Python libraries for access of data generated from IoT devices captured with NuvIoT",
+    license="MIT",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/lagovista/nuvpy",
     packages=setuptools.find_packages(),
     keywords=['NuvIoT','IoT','Devices','Machine Learning'],
-    download_url="https://github.com/lagovista/nuvpy/archive/v_1.0.1.tar.gz",
+    download_url="https://github.com/lagovista/nuvpy/archive/v_1.0.5.tar.gz",
+    install_requires=['jsonschema', 'urllib3', 'requests', 'tqdm', 'tzdata', 'backports.zoneinfo', 'fpdf'], 
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
